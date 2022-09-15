2b3ec4c1839f5cf43c8c0692fcc02083b0664a78
Graham <graham@Stevens-MacBook-Pro-2.local>
2022-09-14 22:45:45 -0700

    button time

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Blurbs from './components/Blurbs';
 import {HouseInfo} from './components/Blurb';
 
 import './App.css';
 
 const App = () => {
-  const houses: HouseInfo[] = [
+  const defaultHouses: HouseInfo[] = [
     {
       name: "Yolohouse",
       blurb: "Yolo is great.",
@@ -15,11 +15,16 @@ const App = () => {
       blurb: "We're a hacienda!",
     }
   ]
+  const [houses, setHouses] = useState<any>(defaultHouses)
+  const onClick = () => {
+    setHouses((prevState:any) => [...prevState, defaultHouses[0]])
+  }
 
   return <>
     <div className="app">
       <Blurbs houses={houses}/>
     </div>
+    <button style={{position:'absolute', top:10, right:10}} onClick={onClick}>click to add</button>
   </>;
 }
 
